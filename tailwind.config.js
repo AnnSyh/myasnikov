6456c6ad204650ba55d04cbd921a5c12defd527f
AnnSyh <ann.syharevskaya@gmail.com>
2025-01-04 16:38:51 +0300

    add fonts and icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js}, /src/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
+	content: [
+		"./src/**/*.{html,js}",
+		"./src/*.{html,js}",
+	],
+  theme: {},
   plugins: [],
 }
 
